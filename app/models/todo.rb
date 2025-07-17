2556395e3da4ea09011b33c8ce9a282487e82e36
Pedro Medina <pedromedina190@gmail.com>
2025-07-17 18:45:53 -0400

    avoid search being cleared on refresh

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -7,7 +7,8 @@ class Todo < ApplicationRecord
   default_scope { order(position: :asc) }
 
   acts_as_list scope: :project
-  broadcasts_to :project
+  # broadcasts_to :project
+  broadcasts_refreshes_to :project
 
   validates :name, presence: true
 
