7d8c299bd2d486c84b0b1a422be2b31077dff19c
Pedro Medina <pedromedina190@gmail.com>
2025-07-21 16:06:43 -0400

    broadcast todo changes to user model, subscribe at my todos view

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -9,6 +9,7 @@ class Todo < ApplicationRecord
   acts_as_list scope: :project
   # broadcasts_to :project
   broadcasts_refreshes_to :project
+  broadcasts_refreshes_to :user
 
   validates :name, presence: true
 
