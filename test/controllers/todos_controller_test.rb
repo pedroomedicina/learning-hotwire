6f0721eee684d777c5e502a84eb445cca3479953
Pedro Medina <pedromedina190@gmail.com>
2025-08-09 23:28:42 -0400

    Integration Tests with Turbo Frames

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "my todos infinite scroll frame" do
+    user = users(:one)
+    sign_in_as user
+
+    todos = []
+    40.times do |i|
+      todos << {name: "Todo #{i}", user_id: user.id}
+    end
+
+    projects(:one).todos.insert_all(todos)
+
+    get todos_path, headers: { "Turbo-Frame" => "x" }
+    assert_select "turbo-frame[id=todos_page_1][target=_top]" do
+      assert_select "turbo-frame[id=todos_page_2][target=_top][loading=lazy][src='/todos?page=2']"
+    end
+
+    get todos_path(page: 2), headers: { "Turbo-Frame" => "x" }
+    assert_select "turbo-frame[id=todos_page_2][target=_top]" do
+      assert_select "turbo-frame[id=todos_page_3][target=_top][loading=lazy]", count: 0
+    end
+  end
 end
