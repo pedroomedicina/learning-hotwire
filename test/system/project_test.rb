89cf43e65c6a65d429a71f4ec66a60d49a768a5b
Pedro Medina <pedromedina190@gmail.com>
2025-08-12 18:52:56 -0400

    System Tests with Turbo Broadcasts

--- test/system/project_test.rb
+++ test/system/project_test.rb
@@ -1,6 +1,8 @@
 require "application_system_test_case"
 
 class ProjectTest < ApplicationSystemTestCase
+    include ActiveJob::TestHelper
+    
     test "adding a todo to a project" do
         sign_in_as users(:one)
         visit project_url(projects(:one))
@@ -9,5 +11,16 @@ class ProjectTest < ApplicationSystemTestCase
         click_button "Create Todo"
         assert_selector "a", text: "New example todo"
         assert_field "Name", with: ""
+    end
+
+    test "refreshes the project when a new todo is broadcast" do
+        project = projects(:one)
+        sign_in_as users(:one)
+        visit project_url(project)
+        assert_turbo_cable_stream_source project, visible: :all
+        assert_selector "a", text: "New example todo", count: 0
+        project.todos.create!(name: "New example todo")
+        perform_enqueued_jobs
+        assert_selector "a", text: "New example todo", count: 1
     end
 end
