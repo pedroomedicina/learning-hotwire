655e4da6b4a731b8adb6884fc8a4b3aef482937f
Pedro Medina <pedromedina190@gmail.com>
2025-08-12 19:28:30 -0400

    Stimulus Drag and Drop System Test

--- test/system/project_test.rb
+++ test/system/project_test.rb
@@ -23,4 +23,28 @@ class ProjectTest < ApplicationSystemTestCase
         perform_enqueued_jobs
         assert_selector "a", text: "New example todo", count: 1
     end
+
+    test "drag and drop" do
+        project = projects(:one)
+        project.todos.delete_all
+        todo1 = project.todos.create!(name: "Todo 1")
+        todo2 = project.todos.create!(name: "Todo 2")
+        todo3 = project.todos.create!(name: "Todo 3")
+
+        assert_equal 1, todo1.position
+        assert_equal 2, todo2.position
+        assert_equal 3, todo3.position
+
+        sign_in_as users(:one)
+        visit project_url(project)
+
+        todo1_element = find("div[id=#{dom_id(todo1)}]")
+        todo2_element = find("div[id=#{dom_id(todo2)}]")
+        todo3_element = find("div[id=#{dom_id(todo3)}]")
+
+        todo1_element.drag_to(todo3_element)
+
+        assert_equal 2, todo3.reload.position
+        assert_equal 3, todo1.reload.position
+    end
 end
